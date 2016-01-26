fa4fac928010a5a603c86bb4b23f717c1e3f3037
廖贤志 <lxz@putao.cn>
2016-01-26 21:22:37 +0800

    first commit

---
+++ PluginDemo/PluginDemo.h
@@ -0,0 +1,13 @@
+//
+//  PluginDemo.h
+//  PluginDemo
+//
+//  Created by xianzhiliao on 16/1/25.
+//  Copyright © 2016年 Putao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PluginDemo : NSObject
+
+@end
---
+++ PluginDemo/PluginDemo.m
@@ -0,0 +1,13 @@
+//
+//  PluginDemo.m
+//  PluginDemo
+//
+//  Created by xianzhiliao on 16/1/25.
+//  Copyright © 2016年 Putao. All rights reserved.
+//
+
+#import "PluginDemo.h"
+
+@implementation PluginDemo
+
+@end
